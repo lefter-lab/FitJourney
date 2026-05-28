29b5464c3b6a356b7402d89888d418b003329620
Todor Lefterov <leftergsm@gmail.com>
2026-05-28 13:00:56 +0300

    feat: implement createProgram method in WorkoutProgramService

--- src/main/java/com/fitjourney/fitjourney/service/WorkoutProgramService.java
+++ src/main/java/com/fitjourney/fitjourney/service/WorkoutProgramService.java
@@ -1,5 +1,8 @@
 package com.fitjourney.fitjourney.service;
 
+import com.fitjourney.fitjourney.dto.WorkoutProgramDto;
+import com.fitjourney.fitjourney.entity.User;
+import com.fitjourney.fitjourney.entity.WorkoutProgram;
 import com.fitjourney.fitjourney.repository.WorkoutProgramRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,5 +12,18 @@ import org.springframework.stereotype.Service;
 public class WorkoutProgramService {
 
     private final WorkoutProgramRepository workoutProgramRepository;
+
+    public void createProgram(WorkoutProgramDto dto, User creator) {
+        WorkoutProgram program = new WorkoutProgram();
+        program.setTitle(dto.getName());
+        program.setDescription(dto.getDescription());
+        program.setDifficulty(dto.getDifficultyLevel());
+        program.setDurationWeeks(dto.getDurationWeeks());
+        program.setPrice(java.math.BigDecimal.valueOf(dto.getPrice()));
+        program.setTrainer(creator);
+        program.setActive(true);
+
+        workoutProgramRepository.save(program);
+    }
 }
 
