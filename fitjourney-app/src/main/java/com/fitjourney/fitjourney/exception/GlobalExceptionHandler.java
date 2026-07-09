e0e7602cc501ef149099aa6fe4b5193f7c803d5f
Todor Lefterov <leftergsm@gmail.com>
2026-07-09 15:48:34 +0300

    feat: implement GlobalExceptionHandler and configure nutrition-svc database

---
+++ fitjourney-app/src/main/java/com/fitjourney/fitjourney/exception/GlobalExceptionHandler.java
@@ -0,0 +1,49 @@
+package com.fitjourney.fitjourney.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({ProgramNotFoundException.class, UserNotFoundException.class, EnrollmentNotFoundException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ModelAndView handleNotFoundExceptions(RuntimeException ex) {
+        log.warn("Resource not found exception caught: {}", ex.getMessage());
+        ModelAndView modelAndView = new ModelAndView("error/404");
+        modelAndView.addObject("message", ex.getMessage());
+        return modelAndView;
+    }
+
+    @ExceptionHandler({UnauthorizedProgramAccessException.class})
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ModelAndView handleForbiddenExceptions(RuntimeException ex) {
+        log.warn("Access denied exception caught: {}", ex.getMessage());
+        ModelAndView modelAndView = new ModelAndView("error/403");
+        modelAndView.addObject("message", ex.getMessage());
+        return modelAndView;
+    }
+
+    @ExceptionHandler({DuplicateEnrollmentException.class, DuplicateReviewException.class, IllegalArgumentException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ModelAndView handleBadRequestExceptions(RuntimeException ex) {
+        log.warn("Bad request exception caught: {}", ex.getMessage());
+        ModelAndView modelAndView = new ModelAndView("error/bad-request");
+        modelAndView.addObject("message", ex.getMessage());
+        return modelAndView;
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ModelAndView handleGlobalExceptions(Exception ex) {
+        log.error("Unexpected error occurred: ", ex);
+        ModelAndView modelAndView = new ModelAndView("error/500");
+        modelAndView.addObject("message", "An unexpected error occurred. Please try again later.");
+        return modelAndView;
+    }
+}
