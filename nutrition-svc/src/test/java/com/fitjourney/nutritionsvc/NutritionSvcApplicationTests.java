a796c7eebe3ed330270f4fb213a945996b83928b
Todor Lefterov <leftergsm@gmail.com>
2026-07-10 17:28:58 +0300

    chore: remove test placeholder comments

--- nutrition-svc/src/test/java/com/fitjourney/nutritionsvc/NutritionSvcApplicationTests.java
+++ nutrition-svc/src/test/java/com/fitjourney/nutritionsvc/NutritionSvcApplicationTests.java
@@ -3,9 +3,6 @@ package com.fitjourney.nutritionsvc;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-// TODO: след лекцията на 20 юли (Unit Testing) — добави реални тестове
-// Минимум: 1 unit test за NutritionService, 1 integration test, 1 API test
-
 @SpringBootTest
 class NutritionSvcApplicationTests {
 
